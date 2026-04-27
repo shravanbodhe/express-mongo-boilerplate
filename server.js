2c040749ef32afe432b3317458dc4bbb0d79fb6d
Shravan Bodhe <shravanbodhe@gmail.com>
2026-04-27 20:50:51 +0530

    CORS added

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const connectDB = require('./config/db');
+const cors = require('cors');
 require('dotenv').config();
 
 
 const app = express();
 
+app.use(cors());
 console.log('$----------------------server Started----------------------$');
 
 
